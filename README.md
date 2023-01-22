6f19a020ef18d5b89a06f23ef32f3f0edfb20026
Diefco <diefco.web@gmail.com>
2023-01-22 10:24:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Minecraft Clon - React & Three.js
+
+Minecraft clone with React and Three.js. Based on Midudev improved course based on the original by FreeCodeCamp.
+
+[Link del curso](https://www.youtube.com/watch?v=dm7nfe3bOE4)
---
+++ src/components/TextureSelector.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import { useKeyboard } from '../hooks/useKeyboard.js'
+import { useStore } from '../hooks/useStore.js'
+import * as images from '../images/images.js'
+
+export const TextureSelector = () => {
+  const [visible, setVisible] = useState(true)
+  const [texture, setTexture] = useStore((state) => [
+    state.texture,
+    state.setTexture
+  ])
+  const { dirt, grass, glass, wood, log } = useKeyboard()
+
+  // useEffect(() => {
+  //   const visibilityTimeOut = setTimeout(() => {
+  //     setVisible(false)
+  //   }, 1000)
+
+  //   setVisible(true)
+
+  //   return () => {
+  //     clearTimeout(visibilityTimeOut)
+  //   }
+  // }, [texture])
+
+  useEffect(() => {
+    const options = {
+      dirt,
+      grass,
+      glass,
+      wood,
+      log
+    }
+
+    const selectedTexture = Object.entries(options).find(
+      ([texture, isEnabled]) => isEnabled
+    )
+
+    if (selectedTexture) {
+      const [textureName] = selectedTexture
+      setTexture(textureName)
+    }
+  }, [dirt, grass, glass, wood, log])
+
+  if (!visible) return null
+
+  return (
+    <div className={`texture-selector ${visible ? '' : 'hidden'}`}>
+      {Object.entries(images).map(([imgKey, img]) => {
+        return (
+          <img
+            className={texture === imgKey.replace('Img', '') ? 'selected' : ''}
+            key={imgKey}
+            src={img}
+            alt={imgKey}
+          />
+        )
+      })}
+    </div>
+  )
+}
